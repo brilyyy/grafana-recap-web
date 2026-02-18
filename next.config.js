317a06bbd594045bc962fedcafc74c12001cc3ba
Anggito02 <anggito_anju@yahoo.com>
2026-02-18 15:41:22 +0700

    [fix] comments

--- next.config.js
+++ next.config.js
@@ -1,32 +1,3 @@
-// /** @type {import('next').NextConfig} */
-// const nextConfig = {
-//   output: 'standalone',
-
-//   experimental: {
-//     instrumentationHook: true,
-
-//     serverComponentsExternalPackages: [
-//       'pg',
-//       'node-cron',
-//       'mysql2',
-//       'typeorm',
-//     ],
-//   },
-
-//   webpack: (config, { isServer }) => {
-//     if (isServer) {
-//       config.externals.push({
-//         'pg-native': 'commonjs pg-native',
-//       })
-//     }
-
-//     return config
-//   },
-// }
-
-// module.exports = nextConfig
-
-
 const nextConfig = {
   output: 'standalone',
 
